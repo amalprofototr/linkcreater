286bd265319c9e2b0a17c1b72d59036e78fcc333
amalprofototr <pa4701234@gmail.com>
2023-11-09 08:18:20 +0530

    fisrt commit

---
+++ webscraper_project/websitescrapapp/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+import requests
+from bs4 import  BeautifulSoup
+from .models import links
+# Create your views here.
+def home(request):
+    if request.method=='POST':
+        linknew=request.POST.get('page','')
+        urls=requests.get(linknew)
+        beautysoup=BeautifulSoup(urls.text,'html.parser')
+
+        for link in beautysoup.find_all('a'):
+            li_address=link.get('href')
+            li_name=link.string
+            links.objects.create(address=li_address,stringname= li_name)
+        return HttpResponseRedirect('/')
+    else:
+
+            data_values=links.objects.all()
+    return render(request,'home.html',{'data_values':data_values})
